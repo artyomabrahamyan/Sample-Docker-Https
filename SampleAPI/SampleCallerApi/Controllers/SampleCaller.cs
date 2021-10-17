0930f19e1f29c17ae9fb4b9d21526b6b9b1f300d
Artyom Abrahamyan <aabrahamyan@servicetitan.com>
2021-10-17 18:50:07 +0400

    Initial commit

---
+++ SampleAPI/SampleCallerApi/Controllers/SampleCaller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SampleCallerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleCaller : ControllerBase
+    {
+        private readonly ILogger<SampleCaller> _logger;
+
+        public SampleCaller(ILogger<SampleCaller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var httpClient = new HttpClient();
+            var result = await httpClient.GetAsync("https://sample.api:443");
+            return Ok(result);
+        }
+    }
+}
