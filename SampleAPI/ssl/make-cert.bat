dbc3c0d178e107e2e09d6a7b1d3d461b6fb67c64
Artyom Abrahamyan <90622536+AAbrahamyan-ST@users.noreply.github.com>
2021-10-24 11:30:25 +0400

    Update make-cert.bat

--- SampleAPI/ssl/make-cert.bat
+++ SampleAPI/ssl/make-cert.bat
@@ -1,4 +1,5 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -config localhost.conf -subj /CN=localhost
 openssl pkcs12 -export -out localhost.pfx -inkey localhost.key -in localhost.crt -passout pass:localhost
-copy localhost.crt ..
+move localhost.crt ..
 copy localhost.pfx %USERPROFILE%\.aspnet\https
+del localhost.key
